61e2ec91817d645b274b4d2f21195034f644146c
David DeFazio <davedefazio1@gmail.com>
2017-06-13 22:31:22 -0400

    produced results from linear regression

--- regression.py
+++ regression.py
@@ -37,39 +37,43 @@ def removeCategories(feature):
 			feature = mergeCategory(feature,category)
 	return feature
 
-#reading data
+def convertNominalToBinary(binary_features,nominal_features):
+
+	#reduce the number of categories for each nominal feature
+	for index,column in enumerate(nominal_features.T):
+		nominal_features.T[index] = removeCategories(column)
+
+	#convert all nominal features into dummy matricies, and append to training data
+	dummy_matrix = np.zeros(len(nominal_features.T[0]),dtype=np.int) #initialize with one column, which will later be removed
+	dummy_matrix = dummy_matrix.reshape(len(dummy_matrix),1)
+
+	#get dummy matrix from each column, and append all dummy matricies together
+	for index,column in enumerate(nominal_features.T):
+		panda_dummy_matrix = pd.get_dummies(nominal_features.T[index])
+		np_matrix = np.asarray(panda_dummy_matrix)
+		for col in np_matrix.T:
+			col = col.reshape(len(col),1)
+			dummy_matrix = np.hstack((dummy_matrix, col))
+
+	dummy_matrix = dummy_matrix[:,1:] #remove first column, which was initialized to all 0's
+
+	X = np.hstack((dummy_matrix,binary_features))
+	X = X.astype(np.int)
+	return X
+
+#reading training data
 with codecs.open('Data/train.csv','r',encoding='utf-8',errors='ignore') as f:
     readCSV = csv.reader(f, delimiter=',')
     data = list(readCSV)
 data = np.array(data)
 
-Y = data[1:,1].reshape(len(data)-1,1).astype(np.float)
 binary_features = data[1:,10:]
 nominal_features = data[1:,2:10]
-
-#reduce the number of categories for each nominal feature
-for index,column in enumerate(nominal_features.T):
-	nominal_features.T[index] = removeCategories(column)
-
-#convert all nominal features into dummy matricies, and append to training data
-dummy_matrix = np.zeros(len(nominal_features.T[0]),dtype=np.int) #initialize with one column, which will later be removed
-dummy_matrix = dummy_matrix.reshape(len(dummy_matrix),1)
-
-for index,column in enumerate(nominal_features.T):
-	panda_dummy_matrix = pd.get_dummies(nominal_features.T[index])
-	np_matrix = np.asarray(panda_dummy_matrix)
-	for col in np_matrix.T:
-		col = col.reshape(len(col),1)
-		dummy_matrix = np.hstack((dummy_matrix, col))
-
-dummy_matrix = dummy_matrix[:,1:]
-
-X = np.hstack((dummy_matrix,binary_features))
-X = X.astype(np.int)
+X_train = convertNominalToBinary(binary_features,nominal_features)
+Y_train = data[1:,1].reshape(len(data)-1,1).astype(np.float)
 
 #at this point, X consists of all relevent features, broken down into binary
 #Y consists of the associated value which we are trying to later predict
-
 
 #############################################  FEATURE SELECTION  ##############################################
 
@@ -77,17 +81,40 @@ X = X.astype(np.int)
 
 #PCA
 pca = decomposition.PCA(n_components=50)
-pca.fit(X)
-X = pca.transform(X)
+pca.fit(X_train)
+X_train = pca.transform(X_train)
 
 
 #############################################  REGRESSION MODELS  ##############################################
 
 #get test data
+with codecs.open('Data/test.csv','r',encoding='utf-8',errors='ignore') as f:
+    readCSV = csv.reader(f, delimiter=',')
+    data = list(readCSV)
+data = np.array(data)
 
-#make single function that takes all input data and returns binary training data and Y
+#transform X_test just as we transformed X_train
+binary_features = data[1:,9:]
+nominal_features = data[1:,1:9]
+X_test = convertNominalToBinary(binary_features,nominal_features)
+pca.fit(X_test)
+X_test = pca.transform(X_test)
 
 #use linear regression model
 model = LinearRegression()
-model.fit(X,Y)
-#prediction = model.predict(training_data_test)
+model.fit(X_train,Y_train)
+prediction = model.predict(X_test)
+
+
+#write submission file
+submission_prediction = prediction.astype(str)
+submission_prediction = submission_prediction.reshape(len(submission_prediction),1)
+ID = data[1:,0]
+ID = ID.reshape(len(ID),1)
+
+submission = np.hstack((ID,submission_prediction))
+
+with open('submission.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['ID','y'])
+    writer.writerows(submission)
